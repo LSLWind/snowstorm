112f5bd7ac1e8a2567f901b8d64402f1b2025b4e
LSLWind <1942557346@qq.com>
2020-09-21 15:38:24 +0800

    init project by springboot

---
+++ src/main/java/lsl/wind/snowstorm/SnowstormApplication.java
@@ -0,0 +1,13 @@
+package lsl.wind.snowstorm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnowstormApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnowstormApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lsl/wind/snowstorm/SnowstormApplicationTests.java
@@ -0,0 +1,13 @@
+package lsl.wind.snowstorm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnowstormApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
